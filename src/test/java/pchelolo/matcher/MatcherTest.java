4ede0830b7725ded061ddae0d7f0b9719c4c87ef
Petr Pchelko <petrpchelko@gmail.com>
2013-07-13 16:52:10 +0400

    NFA modelling and a couple of tests

---
+++ src/test/java/pchelolo/matcher/MatcherTest.java
@@ -0,0 +1,17 @@
+package pchelolo.matcher;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: petrpchelko
+ * Date: 7/13/13
+ * Time: 4:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MatcherTest {
+    @Test
+    public void testMatches() throws Exception {
+
+    }
+}
--- src/test/java/pchelolo/matcher/NFATest.java
+++ src/test/java/pchelolo/matcher/NFATest.java
@@ -1,11 +1,42 @@
 package pchelolo.matcher;
 
-/**
- * Created with IntelliJ IDEA.
- * User: petrpchelko
- * Date: 7/13/13
- * Time: 3:33 PM
- * To change this template use File | Settings | File Templates.
- */
+
+import static org.junit.Assert.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Test;
+import pchelolo.matcher.Pattern;
+import pchelolo.matcher.nfa.NFA;
+import pchelolo.matcher.nfa.NFAConstructionVisitor;
+import pchelolo.matcher.nfa.NFAFragment;
+
 public class NFATest {
+
+    @Test
+    public void NFAConstructionTest() throws Exception {
+        ParseTree tree = Pattern.parse("a|bc");
+        /*
+               'a' -------
+              /           \
+            0 - 'b' - 'c' - - fin
+         */
+        NFAFragment nfa = new NFAConstructionVisitor().visit(tree);
+        assertNotNull(nfa);
+        NFA.State firstState = ;
+        assertNull(firstState.getC());
+        assertNotNull(firstState.getFirstOut());
+        assertNotNull(firstState.getSecondOut());
+        NFA.State aState = firstState.getFirstOut();
+        assertTrue(aState.getC() == 'a');
+        assertNotNull(aState.getFirstOut());
+        assertNull(aState.getSecondOut());
+        NFA.State bState = firstState.getSecondOut();
+        assertTrue(bState.getC() == 'b');
+        assertNull(bState.getSecondOut());
+        NFA.State cState = bState.getFirstOut();
+        assertTrue(cState.getC() == 'c');
+        assertNull(cState.getSecondOut());
+        NFA.State finState = cState.getFirstOut();
+        assertTrue(finState.isFinal());
+        assertEquals(finState, aState.getFirstOut());
+    }
 }
