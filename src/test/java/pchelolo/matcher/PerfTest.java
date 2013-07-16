a9d1a7ab79f05936363fcebef45d71d7ea903b44
Petr Pchelko <petrpchelko@gmail.com>
2013-07-16 23:11:11 +0400

    updated perf test

--- src/test/java/pchelolo/matcher/PerfTest.java
+++ src/test/java/pchelolo/matcher/PerfTest.java
@@ -5,34 +5,57 @@ import org.junit.Test;
 
 public class PerfTest {
 
+    private static final int REPEATS = 20;
+
     @Test
     public void testLongClosure() throws Exception {
-        String pattern = "a*";
+
+        String pattern = "(a|b)*";
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 1000; i++) {
-            sb.append("a");
+        for (int i = 0; i < 100; i++) {
+            sb.append("a").append("b");
         }
         String testCase = sb.append("b").toString();
+
+        java.util.regex.Pattern javaPattern = null;
+        Pattern m = null;
+
+        // Warm up //
+        for (int idx = 1; idx < 2*REPEATS; idx++) {
+            javaPattern = java.util.regex.Pattern.compile(pattern);
+            javaPattern.matcher(testCase).matches();
+            m = Pattern.compile(pattern);
+            m.matcher().matches(testCase);
+        }
+
 
         System.out.println("Long closure test");
         System.out.println("\tJava");
         long start = System.nanoTime();
-        java.util.regex.Pattern javaPattern = java.util.regex.Pattern.compile(pattern);
+        for (int idx = 0; idx < REPEATS; idx ++) {
+            javaPattern = java.util.regex.Pattern.compile(pattern);
+        }
         long end = System.nanoTime();
-        System.out.println("\t\tCompilation:\t" + (end - start));
+        System.out.println("\t\tCompilation:\t" + (end - start) / REPEATS);
         start = System.nanoTime();
-        javaPattern.matcher(testCase).matches();
+        for (int idx = 0; idx < REPEATS; idx ++) {
+            javaPattern.matcher(testCase).matches();
+        }
         end = System.nanoTime();
-        System.out.println("\t\tExecution:\t" + (end - start));
+        System.out.println("\t\tExecution:\t" + (end - start) / REPEATS);
 
         System.out.println("\tCurrent implementation");
         start = System.nanoTime();
-        Pattern m = Pattern.compile(pattern);
+        for (int idx = 0; idx < REPEATS; idx ++) {
+            m = Pattern.compile(pattern);
+        }
         end = System.nanoTime();
-        System.out.println("\t\tCompilation:\t" + (end - start));
+        System.out.println("\t\tCompilation:\t" + (end - start) / REPEATS);
         start = System.nanoTime();
-        m.matcher().matches(testCase);
+        for (int idx = 0; idx < REPEATS; idx ++) {
+            m.matcher().matches(testCase);
+        }
         end = System.nanoTime();
-        System.out.println("\t\tExecution:\t" + (end - start));
+        System.out.println("\t\tExecution:\t" + (end - start) / REPEATS);
     }
 }
