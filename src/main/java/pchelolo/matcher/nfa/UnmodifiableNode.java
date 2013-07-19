9993e657aa6ef8bae3294807de1d29fe72970312
Petr Pchelko <petrpchelko@gmail.com>
2013-07-19 20:22:18 +0400

    merge followup

--- src/main/java/pchelolo/matcher/nfa/UnmodifiableNode.java
+++
@@ -1,9 +0,0 @@
-package pchelolo.matcher.nfa;
-
-public interface UnmodifiableNode {
-    char getC();
-    int getOutCount();
-    int getOut(int index);
-    int getID();
-    boolean isEpsilon();
-}
