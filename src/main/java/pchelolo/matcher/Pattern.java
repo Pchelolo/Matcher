25ff9ee0d4a1d791553a3f5bca4b229a24b54292
Petr Pchelko <petrpchelko@gmail.com>
2013-07-19 20:28:14 +0400

    Serialization

--- src/main/java/pchelolo/matcher/Pattern.java
+++ src/main/java/pchelolo/matcher/Pattern.java
@@ -5,38 +5,48 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.Serializable;
+
 /**
  * Represents a compiled regular expression pattern.
- *
+ * <p/>
  * Immutable.
  */
-public final class Pattern {
+public final class Pattern implements Serializable {
 
-    private final UnmodifiableNFA nfa;
-    // Memory visibility assurance
-    private volatile boolean isCompiled = false;
+    private transient UnmodifiableNFA nfa;
 
-    private Pattern(ParseTree tree) {
-         nfa = new NFAConstructionVisitor().visit(tree);
+    private final String patternString;
+
+    private transient volatile boolean isCompiled = false;
+
+    private Pattern(ParseTree tree, String patternString) {
+        this.patternString = patternString;
+        nfa = new NFAConstructionVisitor().visit(tree);
+        isCompiled = true;
+    }
+
+    private void recompile() {
+        nfa = new NFAConstructionVisitor().visit(parse(patternString));
+        isCompiled = true;
     }
 
     /**
      * Compiles a given pattern into an internal representation
-     *
+     * <p/>
      * The regular expression supports the following syntax constructs.
      * The constructs are given in the order of operator precedence.
-     *
+     * <p/>
      * [abc]   - list          - a shorthand for a|b|b
      * [x..y]  - range         - matches any character from x to y
      * a*      - closure       - mathes 0+ repeats of a given subpattern
      * ab      - concatenation - matches the given sequence of subpatterns
      * a|b     - disjunction   - matches either subpattern a or b
-     *
      */
     public static Pattern compile(final String patternString) {
         ParseTree parseTree = parse(patternString);
         if (parseTree == null) return null;
-        Pattern pattern = new Pattern(parseTree);
+        Pattern pattern = new Pattern(parseTree, patternString);
         pattern.isCompiled = true;
         return pattern;
     }
@@ -46,9 +56,13 @@ public final class Pattern {
      */
     public Matcher matcher(final String testString) {
         if (!isCompiled) {
-            // Impossible
-            throw new IllegalStateException("The pattern was not compiled");
+            synchronized (this) {
+                if (!isCompiled) {
+                    recompile();
+                }
+            }
         }
+
         return new Matcher(nfa, testString, this);
     }
 
